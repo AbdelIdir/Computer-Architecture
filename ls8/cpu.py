a292bebb7a5397c763b61c677c98f9d38f7baa38
AbdelI <homeworkoracle@protonmail.com>
2020-05-20 13:14:03 +0200

    cpu.py,ls8,done

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,22 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.reg = [0] * 8
+        self.ram = [0] * 256
+        self.pc = 0
+        self.halted = False
+
+    def ram_read(self, address):
+        print(self.ram[address])
+
+    def ram_write(self, address, instruction):
+        self.ram[address] = instruction
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +28,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
-            self.ram[address] = instruction
+            self.ram_write(address, instruction)
             address += 1
-
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +57,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -61,5 +70,28 @@ class CPU:
         print()
 
     def run(self):
-        """Run the CPU."""
-        pass
+        while not self.halted:
+            instruction = self.ram[self.pc]
+
+            # LDI - STORE REGISTER VALUE - 10000010
+            if instruction == 0b10000010:
+                value = self.ram[self.pc + 2]
+                register_num = self.ram[self.pc + 1]
+
+                self.reg[register_num] = value
+                self.pc += 3
+
+            # PRN - PRINT REGISTER - 01000111
+            elif instruction == 0b01000111:
+                register_num = self.ram[self.pc + 1]
+                print(self.reg[register_num])
+                self.pc += 2
+
+            # HLT - STOPS - 00000001
+            elif instruction == 0b00000001:
+                self.halted = True
+                self.pc += 1
+
+            else:
+                print(f"Unknown instruction at index {self.pc}")
+                sys.exit(1)
